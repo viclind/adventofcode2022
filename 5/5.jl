8a6ab049f1392e60e7c1b4b098f2b945421dbc50
Victor Lindgren <victor.lindgren.0444@student.lu.se>
2022-12-05 19:55:18 +0100

    day 5

--- 5/5.jl
+++ 5/5.jl
@@ -3,6 +3,22 @@ using DataStructures
 lines = readlines("i.txt")
 stacks = Dict()
 moves = []
+
+function parse_stacks(line)
+    l = [(line[i-2], trunc(Int, i / 4)) for (i,x) in enumerate(line) if (i % 4 == 0 && isletter(line[i-2]))]
+    len = length(line)
+    if (isletter(line[len - 1])) push!(l, (line[len - 1], trunc(Int, (len+1) / 4))) end
+    for x in l
+        if (.!haskey(stacks, x[2]))
+            s = Stack{Char}()
+            push!(s, x[1])
+            stacks[x[2]] = s
+        else
+            push!(stacks[x[2]], x[1])
+        end
+    end
+end
+
 for i in Iterators.reverse(1:length(lines))
     line = lines[i]
     if (occursin("move", line))
@@ -11,27 +27,16 @@ for i in Iterators.reverse(1:length(lines))
         end
         push!(moves, numbers)
     elseif (line != "")
-        l = [(line[i-2], trunc(Int, i / 4)) for (i,x) in enumerate(line) if (i % 4 == 0 && isletter(line[i-2]))]
-        len = length(line)
-        if (isletter(line[len - 1])) push!(l, (line[len - 1], trunc(Int, (len+1) / 4))) end
-        for x in l
-            if (.!haskey(stacks, x[2]))
-                s = Stack{Char}()
-                push!(s, x[1])
-                stacks[x[2]] = s
-            else
-                push!(stacks[x[2]], x[1])
-            end
-        end
+        parse_stacks(line)
         
     end
 end
 
 for move in reverse(moves)
     num_moves = move[1]
-    for num in 1:num_moves
-        box = pop!(stacks[move[2]])
-        push!(stacks[move[3]], box)
+    to_move = [pop!(stacks[move[2]]) for num in 1:num_moves]
+    for crate in reverse(to_move)
+        push!(stacks[move[3]], crate)
     end
 end
 
