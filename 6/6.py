5ec3b9c73091d489cf4aa5700f5aa577181724f9
Victor Lindgren <victor.lindgren.0444@student.lu.se>
2022-12-06 23:16:16 +0100

    day 6

--- 6/6.py
+++ 6/6.py
@@ -1,5 +1,5 @@
 import re
-with open("i.txt") as file:
-    stream = file.read()
-    s = ''.join([i for i in re.findall(r'(?=(\w{14}))', stream) if len(set(i)) == 14][0])
-    print(stream.index(s)+14)
+
+stream = open('i.txt').read()
+s = ''.join([i for i in re.findall(r'(?=(\w{14}))', stream) if len(set(i)) == 14][0])
+print(stream.index(s)+14)
